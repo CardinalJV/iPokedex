40f620c9c9cd068942125f73e3cdd3ab0ce8028c
CardinalJV <jessy.viranaiken@icloud.com>
2024-09-02 18:42:23 +0200

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Jessy Viranaiken on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
