92a305f178c5ab51c0299b0bc75084acd3a18830
CardinalJV <jessy.viranaiken@icloud.com>
2024-09-03 19:51:46 +0200

    Update: Ajout des différentes informations de chaque pokemon

--- Pokedex/Views/PokemonView.swift
+++ Pokedex/Views/PokemonView.swift
@@ -23,19 +23,59 @@ struct PokemonView: View {
           .frame(width: 250)
       }
       .frame(height: 300)
-      HStack{
-        ForEach(pokemon.types) { type in
-          Text(type.name!)
-            .padding(6)
-            .background(pokemon.getColorFromType(type: type.name!))
-            .bold()
-            .clipShape(.rect(cornerRadius: 5))
+      VStack(spacing: 10){
+        HStack{
+          ForEach(pokemon.types) { type in
+            Text(type.name!)
+              .padding(6)
+              .background(pokemon.getColorFromType(type: type.name!))
+              .bold()
+              .clipShape(.rect(cornerRadius: 5))
+          }
         }
-//        Spacer()
+        .padding()
+        /* Composant a propos */
+        VStack{
+          Text("A propos")
+            .bold()
+          HStack(spacing: 50){
+            VStack{
+              HStack{
+                Image(systemName: "scalemass")
+                Text("\(String(format: "%.2f", pokemon.weight)) kg")
+              }
+              Text("Weight")
+            }
+            Divider()
+            VStack{
+              HStack{
+                Image(systemName: "arrow.up.and.down")
+                Text("\(String(format: "%.2f", pokemon.height)) m")
+              }
+              Text("Height")
+            }
+          }
+          .frame(width: 350, height: 75)
+          .background(Color(.systemGray6))
+          .clipShape(.rect(cornerRadius: 10))
+        }
+        /* - */
+        /* Composant statistiques */
+        VStack{
+          Text("Statistiques")
+            .bold()
+          VStack{
+            StatsItem(name: "Hp", color: pokemon.getColorFromType(type: pokemon.types[0].name!), score: pokemon.stats!.hp!)
+            StatsItem(name: "Attaque", color: pokemon.getColorFromType(type: pokemon.types[0].name!), score: pokemon.stats!.atk!)
+            StatsItem(name: "Défense", color: pokemon.getColorFromType(type: pokemon.types[0].name!), score: pokemon.stats!.def!)
+            StatsItem(name: "Attaque !", color: pokemon.getColorFromType(type: pokemon.types[0].name!), score: pokemon.stats!.spe_atk!)
+            StatsItem(name: "Défense !", color: pokemon.getColorFromType(type: pokemon.types[0].name!), score: pokemon.stats!.spe_def!)
+          }
+        }
+        /* - */
+        Spacer()
       }
-      .padding()
-      Spacer()
-    }
+      }
   }
 }
 
