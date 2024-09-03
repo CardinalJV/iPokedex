d435ba3c893bc03633200423d4a09009448692b0
CardinalJV <jessy.viranaiken@icloud.com>
2024-09-03 19:49:06 +0200

    Update: New files

---
+++ Pokedex/Components/StatsItem.swift
@@ -0,0 +1,18 @@
+//
+//  StatsItem.swift
+//  Pokedex
+//
+//  Created by Jessy Viranaiken on 03/09/2024.
+//
+
+import SwiftUI
+
+struct StatsItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatsItem()
+}
