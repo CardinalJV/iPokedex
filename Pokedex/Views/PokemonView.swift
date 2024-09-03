793df0c443095ac448308c30d2aa2701eaac9a5f
CardinalJV <jessy.viranaiken@icloud.com>
2024-09-03 18:25:52 +0200

    Update: UI/UX

--- Pokedex/Views/PokemonView.swift
+++ Pokedex/Views/PokemonView.swift
@@ -1,9 +1,9 @@
-//
-//  PokemonView.swift
-//  Pokedex
-//
-//  Created by Jessy Viranaiken on 02/09/2024.
-//
+  //
+  //  PokemonView.swift
+  //  Pokedex
+  //
+  //  Created by Jessy Viranaiken on 02/09/2024.
+  //
 
 import SwiftUI
 import TyradexKit
@@ -13,7 +13,32 @@ struct PokemonView: View {
   let pokemon: Pokemon
   
   var body: some View {
-    ImageLoader(pokemonImage: pokemon.sprites!.regular)
-      .aspectRatio(contentMode: .fit)
+    VStack(spacing: 0){
+      ZStack{
+        Rectangle()
+          .fill(pokemon.getColorFromType(type: pokemon.types[0].name!).gradient)
+        ImageLoader(image: pokemon.sprites!.regular)
+          .aspectRatio(contentMode: .fit)
+          .shadow(color: .black, radius: 10, x: 0, y: 0)
+          .frame(width: 250)
+      }
+      .frame(height: 300)
+      HStack{
+        ForEach(pokemon.types) { type in
+          Text(type.name!)
+            .padding(6)
+            .background(pokemon.getColorFromType(type: type.name!))
+            .bold()
+            .clipShape(.rect(cornerRadius: 5))
+        }
+//        Spacer()
+      }
+      .padding()
+      Spacer()
+    }
   }
+}
+
+#Preview {
+  LandingView()
 }
