7c630a537cd3175533972d884686509b3e9e9227
CardinalJV <jessy.viranaiken@icloud.com>
2024-09-03 19:50:24 +0200

    Create: Création du composant statistique réutilisables

--- Pokedex/Components/StatsItem.swift
+++ Pokedex/Components/StatsItem.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct StatsItem: View {
+  
+  let name: String
+  let color: Color
+  @State var score: Int
+  let maxScore = 100
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      HStack{
+        Text(name)
+          .frame(width: 100, alignment: .leading)
+        Spacer()
+        Text("\(score)")
+        Spacer()
+        ProgressView(value: Double(score)/Double(maxScore))
+          .progressViewStyle(LinearProgressViewStyle())
+          .tint(color)
+          .frame(width: 100)
+      }
+      .frame(width: 350)
     }
 }
 
 #Preview {
-    StatsItem()
+  StatsItem(name: "HP", color: .blue, score: 25)
 }
