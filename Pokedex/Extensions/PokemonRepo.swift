937d378467ea26df19bb3e18efa439e258fe6196
CardinalJV <jessy.viranaiken@icloud.com>
2024-09-04 18:35:55 +0200

    Update: New files

---
+++ Pokedex/Extensions/PokemonRepo.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonRepo.swift
+//  Pokedex
+//
+//  Created by Jessy Viranaiken on 04/09/2024.
+//
+
+import Foundation
---
+++ Pokedex/Views/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  Pokedex
+//
+//  Created by Jessy Viranaiken on 04/09/2024.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
