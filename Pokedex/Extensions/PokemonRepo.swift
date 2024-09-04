7c8f9be5c9bcd277ef3b4f80f05feef60733bd13
CardinalJV <jessy.viranaiken@icloud.com>
2024-09-04 18:37:29 +0200

    Create: Ajout de la fonction filtre des pokemon dans le type PokemonRepo

--- Pokedex/Extensions/PokemonRepo.swift
+++ Pokedex/Extensions/PokemonRepo.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+import TyradexKit
+
+extension PokemonRepository {
+  func sortPokemons(searchText: String) -> [Pokemon] {
+    return pokemons.filter { $0.name!.fr!.localizedCaseInsensitiveContains(searchText) }
+  }
+}
