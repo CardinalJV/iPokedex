7068388eeb1bd7f8d9e7885e763ff0a602a5cb39
CardinalJV <jessy.viranaiken@icloud.com>
2024-09-05 19:41:26 +0200

    Update: New files

---
+++ Pokedex/Views/FavoritesPokemonsView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesPokemonsView.swift
+//  Pokedex
+//
+//  Created by Jessy Viranaiken on 05/09/2024.
+//
+
+import SwiftUI
+
+struct FavoritesPokemonsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesPokemonsView()
+}
