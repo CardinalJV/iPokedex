ad39af03876a456c64f6b4ed0eb9ae2af1c49e92
CardinalJV <jessy.viranaiken@icloud.com>
2024-09-05 19:47:36 +0200

    Create: Création de la vue des pokémons favoris

--- Pokedex/Views/FavoritesPokemonsView.swift
+++ Pokedex/Views/FavoritesPokemonsView.swift
@@ -6,10 +6,44 @@
 //
 
 import SwiftUI
+import TyradexKit
 
 struct FavoritesPokemonsView: View {
+  
+  @StateObject private var pokemonRepo: PokemonRepository = .shared
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      NavigationStack{
+        VStack{
+          ScrollView{
+            LazyVGrid(columns: [GridItem(.flexible(), spacing: 10), GridItem(.flexible(), spacing: 10)], spacing: 10){
+              ForEach(favoritesPokemons){ pokemon in
+                NavigationLink(destination: PokemonView(pokemon: pokemon)) {
+                  VStack{
+                    ImageLoader(image: pokemon.sprites!.regular)
+                      .aspectRatio(contentMode: .fit)
+                      .shadow(color: .black, radius: 10, x: 0, y: 0)
+                    Text(pokemon.name!.fr!)
+                    Text("#\(pokemon.pokedexID!)")
+                  }
+                  .foregroundStyle(.black)
+                  .padding()
+                  .bold()
+                  .frame(width: 175, height: 175)
+                  .background(pokemon.getColorFromType(type: pokemon.types[0].name!).gradient)
+                  .clipShape(.rect(cornerRadius: 10))
+                }
+              }
+            }
+          }
+        }
+        .padding()
+        .onAppear{
+          Task{
+            await pokemonRepo.fetchPokemons()
+          }
+        }
+      }
     }
 }
 
