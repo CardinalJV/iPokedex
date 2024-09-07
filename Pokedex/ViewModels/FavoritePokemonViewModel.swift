6ca9db906a628df0c08b6f6c183478b322e0549b
CardinalJV <jessy.viranaiken@icloud.com>
2024-09-07 13:38:55 +0200

    Create: Création d'un fichier ViewModel afin de gérer l'état des favoris

--- Pokedex/ViewModels/FavoritePokemonViewModel.swift
+++ Pokedex/ViewModels/FavoritePokemonViewModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import TyradexKit
+
+@Observable
+class FavoritePokemonViewModel {
+  
+  var favoritesPokemons = [Pokemon]()
+  
+  func addInFav(pokemon: Pokemon) async {
+    favoritesPokemons.append(pokemon)
+  }
+  
+  func deleteInFav(pokemon: Pokemon) async {
+    favoritesPokemons.removeAll(where: { $0.id == pokemon.id })
+  }
+  
+  func isInFavorites(pokemon: Pokemon) -> Bool {
+    return favoritesPokemons.contains(where: { $0.id == pokemon.id })
+  }
+}
