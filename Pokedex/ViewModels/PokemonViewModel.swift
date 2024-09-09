be3415851a7deaad922efd473748f176f9acc39b
CardinalJV <jessy.viranaiken@icloud.com>
2024-09-09 18:52:49 +0200

    Delete: Extension PokemonRepo

--- Pokedex/ViewModels/PokemonViewModel.swift
+++ Pokedex/ViewModels/PokemonViewModel.swift
@@ -9,19 +9,29 @@ import Foundation
 import TyradexKit
 
 @Observable
-class FavoritePokemonViewModel {
+class PokemonViewModel {
+  
+  var pokemons = [Pokemon]()
   
   var favoritesPokemons = [Pokemon]()
   
+  func getPokemons(pokemons: [Pokemon]) async {
+    self.pokemons = pokemons
+  }
+  
   func addInFav(pokemon: Pokemon) async {
-    favoritesPokemons.append(pokemon)
+    return favoritesPokemons.append(pokemon)
   }
   
   func deleteInFav(pokemon: Pokemon) async {
-    favoritesPokemons.removeAll(where: { $0.id == pokemon.id })
+    return favoritesPokemons.removeAll(where: { $0.id == pokemon.id })
   }
   
   func isInFavorites(pokemon: Pokemon) -> Bool {
     return favoritesPokemons.contains(where: { $0.id == pokemon.id })
+  }
+  
+  func sortPokemons(searchText: String) -> [Pokemon] {
+    return pokemons.filter { $0.name!.fr!.localizedCaseInsensitiveContains(searchText) }
   }
 }
