59a9db0602f2f8e596891b9227e5f4b5b25a54e4
CardinalJV <jessy.viranaiken@icloud.com>
2024-09-09 18:57:32 +0200

    Update: Modification de la searchBar

--- Pokedex/Views/LandingView.swift
+++ Pokedex/Views/LandingView.swift
@@ -10,45 +10,52 @@ import TyradexKit
 
 struct LandingView: View {
   
-  @State var favoritePokemon_vm = FavoritePokemonViewModel()
   @StateObject private var pokemonRepo: PokemonRepository = .shared
+  @State var pokemon_vm = PokemonViewModel()
+  
   @State private var searchText = ""
   
   var body: some View {
     NavigationStack{
       VStack{
-        TextField("Rechercher un pokémon", text: $searchText)
-          .frame(height: 10)
-          .padding()
-          .background(Color(.systemGray6))
-          .cornerRadius(10)
-        ScrollView{
-          LazyVGrid(columns: [GridItem(.flexible(), spacing: 10), GridItem(.flexible(), spacing: 10)], spacing: 10){
-            ForEach(searchText.isEmpty ? pokemonRepo.pokemons : pokemonRepo.sortPokemons(searchText: self.searchText)){ pokemon in
-              NavigationLink(destination: PokemonView(favoritePokemon_vm: self.favoritePokemon_vm, pokemon: pokemon)) {
-                VStack{
-                  ImageLoader(image: pokemon.sprites!.regular)
-                    .aspectRatio(contentMode: .fit)
-                    .shadow(color: .black, radius: 10, x: 0, y: 0)
-                  Text(pokemon.name!.fr!)
-                  Text("#\(pokemon.pokedexID!)")
+        HStack{
+          TextField("Rechercher un pokémon", text: $searchText)
+          Button(action: { self.searchText.removeAll() }, label: {
+            Image(systemName: "multiply.circle.fill")
+              .tint(.gray)
+          })
+        }
+        .frame(height: 10)
+        .padding()
+        .background(Color(.systemGray6))
+        .cornerRadius(10)
+          ScrollView{
+            LazyVGrid(columns: [GridItem(.flexible(), spacing: 10), GridItem(.flexible(), spacing: 10)], spacing: 10){
+              ForEach(searchText.isEmpty ? pokemon_vm.pokemons : pokemon_vm.sortPokemons(searchText: self.searchText)){ pokemon in
+                NavigationLink(destination: PokemonView(pokemon_vm: self.pokemon_vm, pokemon: pokemon)) {
+                  VStack{
+                    ImageLoader(image: pokemon.sprites!.regular)
+                      .aspectRatio(contentMode: .fit)
+                      .shadow(color: .black, radius: 10, x: 0, y: 0)
+                    Text(pokemon.name!.fr!)
+                    Text("#\(pokemon.pokedexID!)")
+                  }
+                  .foregroundStyle(.black)
+                  .padding()
+                  .bold()
+                  .frame(width: 175, height: 175)
+                  .background(pokemon.getColorFromType().gradient)
+                  .clipShape(.rect(cornerRadius: 10))
                 }
-                .foregroundStyle(.black)
-                .padding()
-                .bold()
-                .frame(width: 175, height: 175)
-                .background(pokemon.getColorFromType(type: pokemon.types[0].name!).gradient)
-                .clipShape(.rect(cornerRadius: 10))
               }
             }
           }
-        }
       }
       .padding()
       .ignoresSafeArea(edges: .bottom)
       .toolbar{
         ToolbarItem(placement: .topBarTrailing) {
-          NavigationLink(destination: FavoritesPokemonsView(favoritePokemon_vm: self.favoritePokemon_vm)) {
+          NavigationLink(destination: FavoritesPokemonsView(pokemon_vm: self.pokemon_vm)) {
             Image(systemName: "heart.fill")
               .font(.title3)
               .tint(.white)
@@ -65,11 +72,10 @@ struct LandingView: View {
       .navigationBarTitleDisplayMode(.inline)
       .toolbarBackground(.red, for: .navigationBar)
       .toolbarBackground(.visible, for: .navigationBar)
-      .onAppear{
+      .task{
         if pokemonRepo.pokemons.isEmpty {
-          Task{
-            await pokemonRepo.fetchPokemons()
-          }
+          await pokemonRepo.fetchPokemons()
+          pokemon_vm.pokemons = pokemonRepo.pokemons
         }
       }
     }
