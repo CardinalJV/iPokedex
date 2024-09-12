5edb322aefc254a94f0abed56eefa17170c75f58
CardinalJV <jessy.viranaiken@icloud.com>
2024-09-12 18:50:28 +0200

    Create: Ajout d'un bouton favoris custom avec animation spécialisé

--- Pokedex/Components/FavoriteButton.swift
+++ Pokedex/Components/FavoriteButton.swift
@@ -1,18 +1,43 @@
-//
-//  FavoriteButton.swift
-//  Pokedex
-//
-//  Created by Jessy Viranaiken on 11/09/2024.
-//
+  //
+  //  FavoriteButton.swift
+  //  Pokedex
+  //
+  //  Created by Jessy Viranaiken on 11/09/2024.
+  //
 
 import SwiftUI
+import TyradexKit
 
 struct FavoriteButton: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  
+  let pokemon_vm: PokemonViewModel
+  let pokemon: Pokemon
+  
+  func image(_ image: Image, show: Bool) -> some View {
+    image
+      .scaleEffect(show ? 1 : 0)
+      .tint(self.pokemon.getColorFromType(type: pokemon.types[0].name!))
+      .bold()
+      .font(.title3)
+      .animation(.easeInOut(duration: 0.3), value: show)
+  }
+  
+  var body: some View {
+    Button(action: {
+      Task {
+        pokemon_vm.isInFavorites(pokemon: self.pokemon) ?
+        await pokemon_vm.deleteInFav(pokemon: self.pokemon) :
+        await pokemon_vm.addInFav(pokemon: self.pokemon)
+      }
+    }, label: {
+      ZStack{
+          image(Image(systemName: "heart.fill"), show: pokemon_vm.isInFavorites(pokemon: pokemon))
+          image(Image(systemName: "heart"), show: !pokemon_vm.isInFavorites(pokemon: pokemon))
+      }
+    })
+  }
 }
 
-#Preview {
-    FavoriteButton()
-}
+  #Preview {
+    LandingView()
+  }
