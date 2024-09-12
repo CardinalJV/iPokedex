53db837457b45580ab46a918bf1207b7d0aa5fbe
CardinalJV <jessy.viranaiken@icloud.com>
2024-09-12 18:46:54 +0200

    Update: New file

---
+++ Pokedex/Components/FavoriteButton.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteButton.swift
+//  Pokedex
+//
+//  Created by Jessy Viranaiken on 11/09/2024.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteButton()
+}
---
+++ Pokedex/Models/PokemonData.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonData.swift
+//  Pokedex
+//
+//  Created by Jessy Viranaiken on 12/09/2024.
+//
+
+import Foundation
