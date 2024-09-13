9145f31a9cfc65f4f76d9fddc2da59f7864ca1ca
CardinalJV <jessy.viranaiken@icloud.com>
2024-09-13 19:23:51 +0200

    Update: Mise en place d'un picker pour sélectionner le type

--- Pokedex/Views/LandingView.swift
+++ Pokedex/Views/LandingView.swift
@@ -12,16 +12,19 @@ struct LandingView: View {
   
   @StateObject private var pokemonRepo: PokemonRepository = .shared
   @State var pokemon_vm = PokemonViewModel()
-  
+
+  private let pokemonTypes = [ "Aucun", "Normal", "Vol", "Plante", "Insecte", "Feu", "Dragon", "Eau", "Glace", "Combat", "Acier", "Poison", "Ténèbres", "Fée", "Spectre", "Sol", "Roche", "Électrik", "Psy" ]
+  @State private var selectedType = "Aucun"
   @State private var searchText = ""
   
   var body: some View {
     NavigationStack{
       VStack{
+        /* SearchBar */
         HStack{
           TextField("Rechercher un pokémon", text: $searchText)
           if !searchText.isEmpty {
-            Button(action: { self.searchText.removeAll() }, label: {
+            Button(action: { self.searchText.removeAll() }, label: {	
               Image(systemName: "multiply.circle.fill")
                 .tint(.gray)
             })
@@ -31,9 +34,33 @@ struct LandingView: View {
         .padding()
         .background(Color(.systemGray6))
         .cornerRadius(10)
+        /* - */
+        /* Picker de type */
+        Menu{
+          Picker("", selection: $selectedType) {
+            ForEach(pokemonTypes, id: \.self) { type in
+              Text(type)
+                .tag(type)
+                .foregroundStyle(.black)
+            }
+          }
+        } label: {
+          HStack {
+            Text(selectedType == "Aucun" ? "Choisir un type" : selectedType)
+            Spacer()
+            Image(systemName: "chevron.down")
+          }
+          .padding()
+          .frame(maxWidth: .infinity, maxHeight: 42.5)
+          .foregroundStyle(selectedType == "Aucun" ? Color(.systemGray2) : .black)
+          .background(selectedType == "Aucun" ? Color(.systemGray6) : pokemon_vm.getColorFromType(selectedType))
+          .cornerRadius(10)
+        }
+        /* - */
+        /* Grille de pokémons */
         ScrollView{
           LazyVGrid(columns: [GridItem(.flexible(), spacing: 10), GridItem(.flexible(), spacing: 10)], spacing: 10){
-            ForEach(searchText.isEmpty ? pokemon_vm.pokemons : pokemon_vm.sortPokemons(searchText: self.searchText)){ pokemon in
+            ForEach(searchText.isEmpty ? pokemon_vm.sortPokemonsByType(selectedType) : pokemon_vm.sortPokemons(searchText: self.searchText)){ pokemon in
               NavigationLink(destination: PokemonView(pokemon_vm: self.pokemon_vm, pokemon: pokemon)) {
                   VStack{
                     ImageLoader(image: pokemon.sprites!.regular)
@@ -46,12 +73,13 @@ struct LandingView: View {
                   .padding()
                   .bold()
                   .frame(width: 175, height: 175)
-                  .background(pokemon.getColorFromType(type: pokemon.types[0].name!).gradient)
+                  .background(pokemon_vm.getColorFromType(pokemon.types[0].name!).gradient)
                   .clipShape(.rect(cornerRadius: 10))
               }
             }
           }
         }
+        /* - */
       }
       .padding()
       .ignoresSafeArea(edges: .bottom)
