e10aebcc030c1b72b16ac40594c9bb17e0dba87b
CardinalJV <jessy.viranaiken@icloud.com>
2024-09-13 19:19:40 +0200

    Update: Deleted files

---
+++ Pokedex/Views/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  Pokedex
+//
+//  Created by Jessy Viranaiken on 13/09/2024.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Test()
+}
